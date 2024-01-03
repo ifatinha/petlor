28bb31fe28648963f40699b3395abcdb5faa0eda
ifatinha <ifatinha99@gmail.com>
2024-01-03 20:06:48 -0300

    Making the mobile menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,14 @@ const menuMobile = document.querySelector(".menu-mobile");
 
 function openMenu(ev) {
     ev.currentTarget.classList.toggle("change");
+
+    const sidenav = document.querySelector(".overlay");
+
+    if (sidenav.style.display === "none") {
+        sidenav.style.display = "block";
+    } else {
+        sidenav.style.display = "none";
+    }
 }
 
 menuMobile.addEventListener("click", openMenu);
