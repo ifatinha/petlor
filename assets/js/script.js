b6a57751dc2a8f45ec6c3b76f7d0408b96711123
ifatinha <ifatinha99@gmail.com>
2024-01-06 19:39:32 -0300

    Ativação do menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,30 +15,33 @@ function openMenu(ev) {
 menuMobile.addEventListener("click", openMenu);
 
 /** stick menu */
-function activeStick() {
-    if (window.scrollY >= stick) {
-        console.log(window.scrollY);
-        navbar.classList.add("stick");
-    } else {
-        navbar.classList.remove("stick");
-    }
-}
-
-window.onscroll = function () { activeStick() }
-const navbar = document.querySelector(".header-menu");
-const stick = navbar.offsetTop;
-
-/** active menu */
-// const linksMenu = document.querySelectorAll(".header-navbar li");
-
-// function activeLinks(link) {
-//     link.addEventListener("click", () => {
-//         link.classList.add("link-active");
-//     })
-
-//     linksMenu.forEach((li) => {
-//         li.classList.remove("link-active");
-//     })
+// function activeStick() {
+//     if (window.scrollY >= stick) {
+//         console.log(window.scrollY);
+//         navbar.classList.add("stick");
+//     } else {
+//         navbar.classList.remove("stick");
+//     }
 // }
 
-// linksMenu.forEach(activeLinks);
+// window.onscroll = function () { activeStick() }
+// const navbar = document.querySelector(".header-menu");
+// const stick = navbar.offsetTop;
+
+/** active menu */
+const linksMenu = document.querySelectorAll(".header-navbar li a");
+
+function activeLink(link, index) {
+
+    link.addEventListener("click", () => {
+        link.parentElement.classList.add("link-active");
+
+        linksMenu.forEach((item, i) => {
+            if (i !== index) {
+                item.parentElement.classList.remove("link-active");
+            }
+        })
+    });
+}
+
+linksMenu.forEach(activeLink);
